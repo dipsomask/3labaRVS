4cd659ae306f968eea905fa42be9d1fc25f1e929
alexe <alexey12345672@yandex.ru>
2023-05-18 20:09:27 +0700

    site without graphics

--- some_site.py
+++ some_site.py
@@ -13,15 +13,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     # получаем файл из запроса
     file = request.files['image']
     if file is not None:
         # читаем изображение с помощью OpenCV
         img = Image.open(BytesIO(file.read()))
+
+        # Resize the image with the new size
+        fixed_width = 500
+        width_percent = (fixed_width / float(img.size[0]))
+        height_size = int((float(img.size[1]) * float(width_percent)))
+        new_size = (fixed_width, height_size)
+        img = img.resize(new_size)
+
         img.save(os.path.join(app.root_path, 'static', 'original.png'))
-        return redirect(url_for('calculus'))
+        return render_template('calculus.html', image_name='original.png')
     else:
         return "didn't open the file"
 
@@ -30,21 +38,30 @@ def upload():
 def calculus():
     if 'number-input' not in request.form:
         return "Number input field not found"
+
     # Get the value of the number input field
     angle = int(request.form['number-input'])
+
     # get the image
     img_path = os.path.join(app.root_path, 'static', 'original.png')
     img = Image.open(img_path)
-    # Rotate the image
+
     # Rotate the image
     rotated = img.rotate(angle)
+
     fixed_width = 500
     width_percent = (fixed_width / float(img.size[0]))
     height_size = int((float(img.size[1]) * float(width_percent)))
+
     # Resize the image with the new size
     new_size = (fixed_width, height_size)
     rotated = rotated.resize(new_size)
-    rotated.save(os.path.join(app.root_path, 'static', 'answer.png'))
+
+    # Save the rotated image with a new name
+    answer_path = os.path.join(app.root_path, 'static', 'answer.png')
+    rotated.save(answer_path)
+
+    # Pass the name of the rotated image to the template
     return render_template('result.html', image_name='answer.png')
 
 
